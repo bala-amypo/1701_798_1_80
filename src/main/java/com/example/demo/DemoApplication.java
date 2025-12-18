51443565e1b909aa801dcd8a2bca826edfbc1638
bala-amypo <bala@amypo.in>
2025-12-18 05:16:57 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,4 @@
-// src/main/java/com/example/demo/DemoApplication.java
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
