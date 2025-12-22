608bc029064c162de7563a387dbbb9cb71fdbcf6
bala-amypo <bala@amypo.in>
2025-12-22 08:24:23 +0000

    User commit

--- src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
+++ src/main/java/com/example/demo/repository/HarmonizedCalendarRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.HarmonizedCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -9,6 +11,6 @@ import java.util.List;
 
 @Repository
 public interface HarmonizedCalendarRepository extends JpaRepository<HarmonizedCalendar, Long> {
-    List<HarmonizedCalendar> findByUserAccountId(Long userAccountId);
-    List<HarmonizedCalendar> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
+        List<HarmonizedCalendar> findByEffectiveFromBetween(LocalDate start, LocalDate end);
+        List<HarmonizedCalendar> findByEffectiveFromLessThanEqualAndEffectiveToGreaterThanEqual(LocalDate date, LocalDate date2);
 }
