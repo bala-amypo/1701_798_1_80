6effe8fe11580eeb335d19cf93b8eef1e15bf018
bala-amypo <bala@amypo.in>
2025-12-24 08:11:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/BranchProfileRepository.java
+++ src/main/java/com/example/demo/repository/BranchProfileRepository.java
@@ -1,8 +1,9 @@
-ackage com.example.demo.repository;
+package com.example.demo.repository;
 
 import com.example.demo.entity.BranchProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 @Repository
