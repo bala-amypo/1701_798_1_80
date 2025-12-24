e59494344fc9824844903f0dfb12bf2adadc099d
bala-amypo <bala@amypo.in>
2025-12-24 03:40:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/repository/AcademicEventRepository.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 @Repository
 public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Long> {
-    List<AcademicEvent> findByBranchId(Long branchId);  // This is correct
-    List<AcademicEvent> findByStartDateBetween(LocalDate start, LocalDate end);
-    List<AcademicEvent> findByEventType(String eventType);
-    List<AcademicEvent> findByBranchIdAndStartDateBetween(Long branchId, LocalDate start, LocalDate end);
+    List<AcademicEvent> findByBranchId(Long branchId);
+    List<AcademicEvent> findByStartDateBetween(LocalDate startDate, LocalDate endDate);
 }
