eaece1a9935e0443348192ad2c08c3b70a59bf40
bala-amypo <bala@amypo.in>
2025-12-26 09:29:04 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import com.example.demo.servlet.SimpleStatusServlet;
 
 @SpringBootApplication
-public class Application {
+public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
