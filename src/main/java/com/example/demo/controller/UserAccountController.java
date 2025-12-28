a0cdc9fe90fed7fe610b207fdac307b5d28fa145
bala-amypo <bala@amypo.in>
2025-12-28 06:30:42 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -156,6 +156,64 @@ public class UserAccountController {
 //         return ResponseEntity.ok(response);
 //     }
 
+package com.example.demo.controller;
+
+import com.example.demo.dto.*;
+import com.example.demo.entity.UserAccount;
+
+import com.example.demo.service.UserAccountService;
+import com.example.demo.security.JwtUtil;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserAccountController {
+
+    @Autowired
+    private UserAccountService userAccountService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    
+    @PostMapping("/register")
+    public UserAccount registerUser(@RequestBody UserAccount user) {
+        return userAccountService.register(user);
+    }
+//     @PostMapping("/register")
+//     public ResponseEntity<AuthResponse> registerUser(
+//             @RequestBody RegisterRequest request) {
+
+//         UserAccount user = new UserAccount();
+//         user.setFullName(request.getFullName());
+//         user.setEmail(request.getEmail());
+//         user.setPassword(request.getPassword());
+//         user.setRole(request.getRole());
+//         user.setDepartment(request.getDepartment());
+
+//         UserAccount savedUser = userAccountService.registerUser(user);
+//         String email = request.getEmail();
+//         // ✅ FIX IS HERE
+//         String token = jwtUtil.generateToken(
+//         Map.of("email", email),
+//         email
+// );
+
+
+//         AuthResponse response = new AuthResponse(
+//                 token,
+//                 savedUser.getEmail(),
+//                 savedUser.getRole()
+//         );
+
+//         return ResponseEntity.ok(response);
+//     }
+
+
+
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> loginUser(
             @RequestBody LoginRequest request) {
